c913aafcec36101a559675eca102a4c1049656ec
S Raghavendra <suradaraghavendra@gmail.com>
2026-03-24 21:28:53 +0530

    Initial commit

---
+++ day5.py
@@ -0,0 +1,29 @@
+'''String ---> String is a collection or sequence of characters, which represented by " " or '' and we can access the using indexing (string can also allow negative indexing) and also slicing. this also immutable'''
+
+'''k=int(input())
+any='Python Programming'
+print(any[k]'''
+
+
+'''
+name="My name is Surada Raghavendra.This is my day 5 of learning python."
+print(name[2:8])'''
+
+'''len()---> len() method is used to get char present in the string or find the  length of the string
+
+Note:- we can convert a string into integer, if the contain only integer values
+
+syntax--->variable_name.split("substring")
+'''
+'''some="Python is a programming language"
+print(some.split(" ")) '''
+
+
+
+'''lower()--->this is used to convert all letter into lower case'''
+some="Python IS a programMing LAnguage"
+print(some.lower())
+ 
+
+
+
