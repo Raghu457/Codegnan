f5bfe6d351534a73ab4571af7297f3f6505d2cfa
S Raghavendra <suradaraghavendra@gmail.com>
2026-03-25 19:43:40 +0530

    day6.py

---
+++ day6.py
@@ -0,0 +1,55 @@
+#identifying vowels and consonants
+''' a=input("Enter a letter")
+if a.lower() in "aeiou":
+    print("VOWELS")
+
+else:
+    print("CONSONANTS")'''
+
+#list
+''' list---> 
+
+list_num=[1,2,3,"pyhton",[4,5,["java","programming"],"language"]]
+print(list_num[4][3][4])'''
+
+'''
+n=[3,4,5,"python",[418,5,8,["language"],43]]
+print(n[4][3][0][1])'''
+
+#append
+'''list_name=[1,2,3]
+print(list_name)
+list_name.append([0,4])
+print(list_name)'''
+
+
+#extend
+'''
+list_num=[1,2,"dimple"]
+list_num.extend([213,4,5])
+print(list_num)
+
+list_num=[1,2,"dimple"]
+list_num.extend("raghu"])
+print(list_num)'''
+
+
+#remove
+'''
+lis_num=[1,2,3,"dimpi"]
+lis_num.remove("dimpi")
+print(lis_num)'''
+
+
+#pop
+
+list_num=[1,2,3,"avanthi"]
+print(list_num)
+list_num.pop(3)
+print(list_num)
+
+
+
+
+
+
